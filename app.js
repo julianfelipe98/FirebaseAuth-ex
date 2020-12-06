e03160b238edc197796ec09c764630b89c40e0b8
julianfelipe98 <juliancho741@hotmail.com>
2020-12-06 14:31:11 -0500

    añadiendo info al readme y corrigiendo error en implementacion del script firebase

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    res.render("index",{name:"julio"});
+    res.render("index");
 })
 
 app.listen(port, () => {
